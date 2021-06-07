7f47f1f375430eff767f98eefeb41159a37e373f
Glenn Austin <glenn@austinsoft.com>
2021-06-06 23:21:16 -0700

    Initial commit for dovecot-gla, which is dovecot + MariaDB support

---
+++ dovecot-gla.rb
@@ -0,0 +1,113 @@
+class DovecotGla < Formula
+  desc "IMAP/POP3 server"
+  homepage "https://dovecot.org/"
+  url "https://dovecot.org/releases/2.3/dovecot-2.3.14.tar.gz"
+  sha256 "c8b3d7f3af1e558a3ff0f970309d4013a4d3ce136f8c02a53a3b05f345b9a34a"
+  license all_of: ["BSD-3-Clause", "LGPL-2.1-or-later", "MIT", "Unicode-DFS-2016", :public_domain]
+
+  livecheck do
+    url "https://dovecot.org/download"
+    regex(/href=.*?dovecot[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+  
+  conflicts_with "dovecot", because: "This is dovecot with optional support for MariaDB"
+
+  depends_on "openssl@1.1"
+  depends_on "mariadb" => :optional
+
+  uses_from_macos "bzip2"
+  uses_from_macos "sqlite"
+
+  on_linux do
+    depends_on "linux-pam"
+  end
+
+  resource "pigeonhole" do
+    url "https://pigeonhole.dovecot.org/releases/2.3/dovecot-2.3-pigeonhole-0.5.14.tar.gz"
+    sha256 "68ca0f78a3caa6b090a469f45c395c44cf16da8fcb3345755b1ca436c9ffb2d2"
+  end
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --libexecdir=#{libexec}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --with-bzlib
+      --with-pam
+      --with-sqlite
+      --with-ssl=openssl
+      --with-zlib
+    ]
+    
+    if build.with? "mariadb"
+      args.append("--with-mysql")
+      ENV['CPATH']='/usr/local/opt/mariadb/include/mysql'
+    end
+
+    system "./configure", *args
+    system "make", "install"
+
+    resource("pigeonhole").stage do
+      args = %W[
+        --disable-dependency-tracking
+        --with-dovecot=#{lib}/dovecot
+        --prefix=#{prefix}
+      ]
+
+      system "./configure", *args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      For Dovecot to work, you may need to create a dovecot user
+      and group depending on your configuration file options.
+    EOS
+  end
+
+  plist_options startup: true
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>KeepAlive</key>
+          <false/>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_sbin}/dovecot</string>
+            <string>-F</string>
+          </array>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/dovecot/dovecot.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/dovecot/dovecot.log</string>
+          <key>SoftResourceLimits</key>
+          <dict>
+          <key>NumberOfFiles</key>
+          <integer>1000</integer>
+          </dict>
+          <key>HardResourceLimits</key>
+          <dict>
+          <key>NumberOfFiles</key>
+          <integer>1024</integer>
+          </dict>
+        </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{sbin}/dovecot --version")
+  end
+end
